bcd529d7451c478efdbb38ff44f35c15c8bed16a
klevi05 <klevi.dervishi@gmail.com>
2021-05-02 23:28:51 +0200

    Third attempt

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
-const mongoose = require('mongoose');
+//const cors = require('cors')
+//const mongoose = require('mongoose');
 //const post = require('./routes/posts')
 const First = require('./routes/first')
 require('dotenv').config();
 
-mongoose.connect(process.env.ACCES_DATABASE_LINK, { useNewUrlParser : true, useUnifiedTopology: true } ,
-    ()=>
-    console.log("Connected succesfull")
-);
+//mongoose.connect(process.env.ACCES_DATABASE_LINK, { useNewUrlParser : true, useUnifiedTopology: true } ,
+  //  ()=>
+    //console.log("Connected succesfull")
+//);
 
-app.use(cors());
-app.use(express.json());
+//app.use(cors());
+//app.use(express.json());
 app.use('/', First)
 //app.use('/user', post);
 
