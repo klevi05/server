13061e60f4bd66a09150e86fe54cf9aa1b72faf0
klevi05 <klevi.dervishi@gmail.com>
2021-05-02 23:30:58 +0200

    Fourth atemmpte

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-//const cors = require('cors')
+const cors = require('cors')
 //const mongoose = require('mongoose');
 //const post = require('./routes/posts')
 const First = require('./routes/first')
@@ -11,8 +11,8 @@ require('dotenv').config();
     //console.log("Connected succesfull")
 //);
 
-//app.use(cors());
-//app.use(express.json());
+app.use(cors());
+app.use(express.json());
 app.use('/', First)
 //app.use('/user', post);
 
