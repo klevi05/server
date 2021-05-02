cc2dd82c8bca3492ebfa0e947d40f1d686e126f6
klevi05 <klevi.dervishi@gmail.com>
2021-05-02 23:32:36 +0200

    Fifth atemmpte

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-//const mongoose = require('mongoose');
-//const post = require('./routes/posts')
+const mongoose = require('mongoose');
+const post = require('./routes/posts')
 const First = require('./routes/first')
 require('dotenv').config();
 
-//mongoose.connect(process.env.ACCES_DATABASE_LINK, { useNewUrlParser : true, useUnifiedTopology: true } ,
-  //  ()=>
-    //console.log("Connected succesfull")
-//);
+mongoose.connect(process.env.ACCES_DATABASE_LINK, { useNewUrlParser : true, useUnifiedTopology: true } ,
+   ()=>
+    console.log("Connected succesfull")
+);
 
 app.use(cors());
 app.use(express.json());
 app.use('/', First)
-//app.use('/user', post);
+app.use('/user', post);
 
 app.listen(process.env.PORT, ()=>{
     console.log('Succses')
